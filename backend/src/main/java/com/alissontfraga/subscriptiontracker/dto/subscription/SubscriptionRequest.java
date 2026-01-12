d4c3fd4fce8ea4535900f399ce6f97ba233d3c8d
alissontfraga <alissontfraga@gmail.com>
2026-01-11 23:04:36 -0300

    refactor(dto): add Category enum

--- backend/src/main/java/com/alissontfraga/subscriptiontracker/dto/subscription/SubscriptionRequest.java
+++ backend/src/main/java/com/alissontfraga/subscriptiontracker/dto/subscription/SubscriptionRequest.java
@@ -3,29 +3,35 @@ package com.alissontfraga.subscriptiontracker.dto.subscription;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import com.alissontfraga.subscriptiontracker.enums.Category;
 import com.alissontfraga.subscriptiontracker.enums.Currency;
 import com.alissontfraga.subscriptiontracker.enums.Frequency;
 import com.alissontfraga.subscriptiontracker.enums.Status;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record SubscriptionRequest(
 
-    @NotNull
-    String name,
-    @NotNull
-    BigDecimal price,
-    @NotNull
-    Currency currency,
-    @NotNull
-    Frequency frequency,
-    @NotNull
-    Status status,
-    @NotNull
-    LocalDate startDate,
-    @NotNull
-    LocalDate renewalDate
-    
-) {
-    
-}
+        @NotBlank
+        String name,
+
+        @NotNull
+        BigDecimal price,
+
+        @NotNull
+        Currency currency,
+
+        @NotNull
+        Frequency frequency,
+
+        @NotNull
+        Category category,
+
+        @NotNull
+        Status status,
+
+        LocalDate startDate,
+        
+        LocalDate renewalDate
+) {}
--- backend/src/main/java/com/alissontfraga/subscriptiontracker/dto/subscription/SubscriptionResponse.java
+++ backend/src/main/java/com/alissontfraga/subscriptiontracker/dto/subscription/SubscriptionResponse.java
@@ -3,26 +3,34 @@ package com.alissontfraga.subscriptiontracker.dto.subscription;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import com.alissontfraga.subscriptiontracker.entity.Subscription;
+import com.alissontfraga.subscriptiontracker.enums.Category;
 import com.alissontfraga.subscriptiontracker.enums.Currency;
 import com.alissontfraga.subscriptiontracker.enums.Frequency;
 import com.alissontfraga.subscriptiontracker.enums.Status;
 
 public record SubscriptionResponse(
-    Long id,
-
-    String name,
-
-    BigDecimal price,
-
-    Currency currency,
-
-    Frequency frequency,
-
-    Status status,
-
-    LocalDate startDate,
-
-    LocalDate renewalDate,
-
-    Long ownerId
-) {}
+        Long id,
+        String name,
+        BigDecimal price,
+        Currency currency,
+        Frequency frequency,
+        Category category,
+        Status status,
+        LocalDate startDate,
+        LocalDate renewalDate
+) {
+    public SubscriptionResponse(Subscription subscription) {
+        this(
+            subscription.getId(),
+            subscription.getName(),
+            subscription.getPrice(),
+            subscription.getCurrency(),
+            subscription.getFrequency(),
+            subscription.getCategory(),
+            subscription.getStatus(),
+            subscription.getStartDate(),
+            subscription.getRenewalDate()
+        );
+    }
+}
