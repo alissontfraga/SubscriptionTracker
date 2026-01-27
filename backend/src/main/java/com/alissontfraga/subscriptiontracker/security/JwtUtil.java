8f5e964ba12b9f834090ca34d937eace37c85eb2
alissontfraga <alissontfraga@gmail.com>
2026-01-27 12:41:10 -0300

    refactor(security): improve getRoles method from JwtUtil

--- backend/src/main/java/com/alissontfraga/subscriptiontracker/security/JwtUtil.java
+++ backend/src/main/java/com/alissontfraga/subscriptiontracker/security/JwtUtil.java
@@ -2,6 +2,7 @@ package com.alissontfraga.subscriptiontracker.security;
 
 import java.time.Instant;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -43,6 +44,11 @@ public class JwtUtil {
     public String getUsername(String token) {
         return verify(token).getSubject();
     }
+
+    public List<String> getRoles(String token) {
+    return verify(token).getClaim(ROLES_CLAIM).asList(String.class);
+}
+
 
     public boolean validate(String token) {
         try {
